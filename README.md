e3b95077cac1334f9b11519f6aa6b976b3997aca
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 22:45:37 -0700

    part 1

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # CSE110 SP21 Lab 5
 
 ## Author(s):
-- YOUR NAME
+- Derek Hwang
+- Michael Shao
 
 ## Part 1:
 
--- script.js
+++ script.js
@@ -2,14 +2,199 @@
 
 const img = new Image(); // used to load image from <input> and draw to canvas
 
+var btn = document.getElementsByTagName('button'); // Ref to the buttons
+
+const myCanvas = document.getElementById('user-image');
+
+const ctx = myCanvas.getContext('2d');
+
+var speechSyn = window.speechSynthesis;
+
+var voiceSec = document.getElementById('voice-selection');
+var volume = 100;
+
+var speak1 = undefined;
+var speak2 = undefined;
+
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
-  // TODO
+  console.log('loading image');
+  
+  // toggle relevant buttons
+  btn[0].disabled = false; // Generate
+   
+  btn[1].disabled = true; // Clear
+  btn[2].disabled = true; // Read Text
+
+  // get Dimensions
+  let imgDimensions = getDimensions(myCanvas.width, myCanvas.height, img.width, img.height);
+  let canvasWidth = imgDimensions['width'];
+  let canvasHeight = imgDimensions['height'];
+  let canvasStartX = imgDimensions['startX'];
+  let canvasStartY = imgDimensions['startY'];
+
+  // fill with black
+  ctx.rect(0,0,myCanvas.width,myCanvas.height);
+  ctx.fillStyle = 'black';
+  ctx.fill();
+
+  // draw image
+  ctx.drawImage(img, canvasStartX, canvasStartY, canvasWidth, canvasHeight);
+
+  document.getElementById('image-input').value = null;
+  document.getElementById('text-top').value = "";
+  document.getElementById('text-bottom').value = "";
 
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
   // - Clear the form when a new image is selected
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
+});
+
+
+const fileSelect = document.getElementById("image-input");
+fileSelect.addEventListener('change', () => {
+  let file = document.getElementById('image-input').files[0]
+
+  //img.src = 'images/' + file.name;
+  img.src = URL.createObjectURL(file); //allows the use of any image, not just in the images directory
+  img.alt = file.name; 
+});
+
+const submitBtn = btn[0];
+submitBtn.addEventListener('click',() =>{
+  let s = myCanvas.width/5;
+  ctx.font = s + "px Comic Sans MS";
+  ctx.fillStyle = "white"
+  ctx.textAlign ='center';
+  ctx.fillText(document.getElementById('text-top').value,myCanvas.width/2,myCanvas.height * 2/10);
+  ctx.fillText(document.getElementById('text-bottom').value,myCanvas.width/2,myCanvas.height * 9/10);
+
+  //document.getElementById('text-top').value = "";
+  //document.getElementById('text-bottom').value = "";
+  
+  btn[0].disabled = true;
+  btn[1].disabled = false;
+  btn[2].disabled = false;
+
+  // populate voices
+  
+  voiceSec.disabled = false;
+  voiceSec.remove(0);
+
+  /*let voicesList = speechSyn.getVoices();
+
+  voicesList.map((v) => {
+    let option = document.createElement('option');
+    option.text = v.name;
+    voiceSec.add(option);
+  })*/
+
+  speak1 = new SpeechSynthesisUtterance(document.getElementById('text-top').value);
+  speak2 = new SpeechSynthesisUtterance(document.getElementById('text-bottom').value);
+
+  let voices = speechSyn.getVoices();
+
+  for(var i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSec.add(option);
+  }
+});
+
+// can't clear text when there's no image on canvas
+const clearBtn = btn[1];
+clearBtn.addEventListener('click', () => {
+  console.log('clearing image');
+
+  // clear rectangle
+  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+
+  // clear image
+  img.src = "";
+  img.alt = "";
+
+  // clear text
+  //document.getElementById('text-top').value = "";
+  //document.getElementById('text-bottom').value = "";
+
+  // enable gen, disable clear, disable read
+  btn[0].disabled = false;
+  btn[1].disabled = true;
+  btn[2].disabled = true;
+
+  window.speechSynthesis.cancel();
+});
+
+const readBtn = btn[2];
+readBtn.addEventListener('click', () => { 
+  let voices = speechSyn.getVoices(); 
+  
+  var selectedOption = voiceSec.selectedOptions[0].getAttribute('data-name');
+    for(var i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        speak1.voice = voices[i];
+        speak2.voice = voices[i];
+      }
+    }
+
+  
+    speak1.volume = volume/100; 
+    speak2.volume = volume/100; 
+
+    speechSyn.speak(speak1);
+    speechSyn.speak(speak2);
+
+    console.log('finished speaking');
+  
+    //inputTxt.blur();
+});
+
+const volGroup = document.getElementById("volume-group");
+volGroup.addEventListener('input', () =>{
+  volume = document.querySelector('div > input').value;
+  let vImage = document.getElementsByTagName('img')[0];
+  /*
+  volume-level-3: 67-100
+  volume-level-2: 34-66
+  volume-level-1: 1-33
+  volume-level-0: 0
+ 
+  */
+  
+  
+  if (volume >= 67 && volume <= 100){
+    vImage.src = "icons/volume-level-3.svg";
+    vImage.alt = "Volume Level 3";
+
+    //console.log(volume);
+    //console.log (vImage.src);
+  } else if (volume <= 66 && volume >= 34){
+    vImage.src = "icons/volume-level-2.svg";
+    vImage.alt = "Volume Level 2";
+    
+    //console.log(volume);
+    //console.log (vImage.src);
+  } else if (volume <= 33 && volume >= 1) {
+    vImage.src = "icons/volume-level-1.svg";
+    vImage.alt = "Volume Level 1";
+    
+    //console.log(volume);
+    //console.log (vImage.src);
+  } else {
+    vImage.src = "icons/volume-level-0.svg"
+    vImage.alt = "Volume Level 0";
+    
+    //console.log(volume);
+    //console.log (vImage.src);
+  }
 });
 
 /**
@@ -23,7 +208,7 @@ img.addEventListener('load', () => {
  * and also the starting X and starting Y coordinate to be used when you draw the new image to the
  * Canvas. These coordinates align with the top left of the image.
  */
-function getDimmensions(canvasWidth, canvasHeight, imageWidth, imageHeight) {
+function getDimensions(canvasWidth, canvasHeight, imageWidth, imageHeight) {
   let aspectRatio, height, width, startX, startY;
 
   // Get the aspect ratio, used so the picture always fits inside the canvas
